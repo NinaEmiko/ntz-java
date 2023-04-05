7a27fca9699c69a01a4b62d5bfa3330018066bc6
Nina <nina.emiko.mcnair@gmail.com>
2023-04-05 14:35:07 -0400

    Have -r save message into general

--- ntz/src/main/java/rocks/zipcode/Notez.java
+++ ntz/src/main/java/rocks/zipcode/Notez.java
@@ -12,7 +12,7 @@ public final class Notez {
     }
     /**
      * Says hello to the world.
-     * 
+     *
      * @param args The arguments of the program.
      */
     public static void main(String argv[]) {
@@ -32,14 +32,12 @@ public final class Notez {
 
         /*
          * You will spend a lot of time right here.
-         * 
+         *
          * instead of loadDemoEntries, you will implement a series
          * of method calls that manipulate the Notez engine.
          * See the first one:
          */
-        ntzEngine.loadDemoEntries();
-
-        ntzEngine.saveDatabase();
+        //ntzEngine.loadDemoEntries();
 
         if (argv.length == 0) { // there are no commandline arguments
             //just print the contents of the filemap.
@@ -53,10 +51,12 @@ public final class Notez {
         /*
          * what other method calls do you need here to implement the other commands??
          */
+        ntzEngine.saveDatabase();
 
     }
 
     private void addToCategory(String string, String[] argv) {
+        filemap.get(string).add(argv[1]);
     }
 
     private void saveDatabase() {
